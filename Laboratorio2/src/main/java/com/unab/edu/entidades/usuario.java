e7bc67ddc84a7634dfe86ed774dcfce3497a7934
FattyC24 <62962069+FattyC24@users.noreply.github.com>
2020-10-24 16:55:39 -0600

    Creacion de Entidades
    Creacion de Procedimientos Almacenados y Entidades

---
+++ Laboratorio2/src/main/java/com/unab/edu/entidades/usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.entidades;
+
+
+/**
+ *
+ * @author CruzCalles
+ */
+
+public class usuario extends tipoUsuario{
+    protected int idUsuario;
+    protected String Usuario;
+    protected String Pass;
+    int tipoUsuario;
+    
+}
